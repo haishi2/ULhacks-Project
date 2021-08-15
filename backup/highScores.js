8d37e300712f8e3f853955599f48d6209bd4069d
Biggeroof <58992834+Biggeroof@users.noreply.github.com>
2021-08-14 21:38:03 -0400

    Delete backup directory

--- backup/highScores.js
+++
@@ -1,9 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-  .map(score => {
-    //returns list item from the score object with formatting of the high-score class
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
